df3b968feba8e6c59b60ede1bdc91d501dd8d61c
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-18 15:17:26 +0300

    Create ScriptedMonster.hx

---
+++ source/utf/objects/battle/ScriptedMonster.hx
@@ -0,0 +1,7 @@
+package utf.objects.battle;
+
+/**
+ * A monster that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedMonster extends utf.objects.battle.Monster implements polymod.hscript.HScriptedClass {}
