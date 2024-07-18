69dfd64bf3fc6556fdc2d51c6dd968795f9667e3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-18 17:45:52 +0300

    Update Monster.hx

--- source/utf/objects/battle/Monster.hx
+++ source/utf/objects/battle/Monster.hx
@@ -1,9 +1,6 @@
 package utf.objects.battle;
 
-import utf.backend.AssetPaths;
 import flixel.group.FlxSpriteGroup;
-import haxe.Json;
-import openfl.utils.Assets;
 
 /**
  * Data structure representing attributes of a monster.
@@ -14,26 +11,32 @@ typedef MonsterData =
 	 * The name of the monster.
 	 */
 	name:String,
+
 	/**
 	 * Current health points of the monster.
 	 */
 	hp:Int,
+
 	/**
 	 * Maximum health points of the monster.
 	 */
 	maxHp:Int,
+
 	/**
 	 * Attack power of the monster.
 	 */
 	attack:Float,
+
 	/**
 	 * Defense power of the monster.
 	 */
 	defense:Float,
+
 	/**
 	 * Experience points reward for defeating the monster.
 	 */
 	xpReward:Int,
+
 	/**
 	 * Gold reward for defeating the monster.
 	 */
@@ -46,36 +49,19 @@ typedef MonsterData =
 class Monster extends FlxSpriteGroup
 {
 	/**
-	 * Data structure to hold monster attributes.
+	 * The ID of the monster.
 	 */
-	public var data(default, null):MonsterData;
+	public var monsterId:String;
 
 	/**
 	 * Constructor to initialize the monster with specified attributes.
 	 *
-	 * @param x The x-coordinate position.
-	 * @param y The y-coordinate position.
-	 * @param name The name of the monster.
+	 * @param monsterId The ID of the monster.
 	 */
-	public function new(x:Float = 0, y:Float = 0, name:String):Void
+	public function new(monsterId:String):Void
 	{
-		super(x, y);
+		super();
 
-		// Load monster data from assets based on the provided name.
-		if (Assets.exists(AssetPaths.data('monsters/$name')))
-			data = Json.parse(Assets.getText(AssetPaths.data('monsters/$name')));
-		else
-		{
-			// Default values in case data loading fails.
-			data = {
-				name: 'Error',
-				hp: 50,
-				maxHp: 50,
-				attack: 0,
-				defense: 0,
-				xpReward: 0,
-				goldReward: 0
-			};
-		}
+		this.monsterId = monsterId;
 	}
 }
