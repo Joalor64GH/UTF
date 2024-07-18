e178c03ecc6f5059a51a7f9c92e8327c26b06065
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-18 11:41:12 +0300

    Update Mods.hx

--- source/backend/Mods.hx
+++ source/backend/Mods.hx
@@ -1,14 +1,19 @@
 package backend;
 
 #if polymod
+import flixel.math.FlxPoint;
 import flixel.util.FlxSave;
 import flixel.FlxG;
 import polymod.backends.PolymodAssets;
+import polymod.format.ParseRules;
 import polymod.util.VersionUtil;
 import polymod.Polymod;
 import openfl.Lib;
 import sys.FileSystem;
 
+/**
+ * @see https://github.com/FunkinCrew/Funkin/blob/main/source/funkin/modding/PolymodHandler.hx
+ */
 class Mods
 {
 	public static var data(default, null):Map<String, ModMetadata> = [];
@@ -30,6 +35,8 @@ class Mods
 			}
 		}
 
+		buildImports();
+
 		if (!FileSystem.exists('mods'))
 			FileSystem.createDirectory('mods');
 
@@ -39,6 +46,9 @@ class Mods
 			framework: OPENFL,
 			ignoredFiles: Polymod.getDefaultIgnoreList(),
 			extensionMap: ['frag' => TEXT, 'vert' => TEXT],
+			parseRules: buildParseRules(),
+			useScriptedClasses: true,
+			loadScriptsAsync: #if html5 true #else false #end,
 			apiVersionRule: VersionUtil.anyPatch(Lib.application.meta.get('version'))
 		});
 	}
@@ -48,7 +58,7 @@ class Mods
 		if (data != null && Lambda.count(data) > 0)
 			data.clear();
 
-		var packs:Array<String> = [];
+		final packs:Array<String> = [];
 
 		for (pack in Polymod.scan({modRoot: 'mods'}))
 		{
@@ -60,6 +70,45 @@ class Mods
 		}
 
 		return packs;
+	}
+
+	private static function buildImports():Void
+	{
+		Polymod.addImportAlias('flixel.math.FlxPoint', .FlxBasePoint);
+
+		Polymod.blacklistImport('Sys');
+		Polymod.blacklistImport('Reflect');
+		Polymod.blacklistImport('Type');
+
+		#if cpp
+		Polymod.blacklistImport('cpp.Lib');
+		#end
+
+		for (cls in ClassMacro.listClassesInPackage('polymod'))
+		{
+			if (cls == null)
+				continue;
+
+			Polymod.blacklistImport(Type.getClassName(cls));
+		}
+
+		#if sys
+		for (cls in ClassMacro.listClassesInPackage('sys'))
+		{
+			if (cls == null)
+				continue;
+
+			Polymod.blacklistImport(Type.getClassName(cls));
+		}
+		#end
+	}
+
+	private static function buildParseRules():ParseRules
+	{
+		final output:polymod.format.ParseRules = polymod.format.ParseRules.getDefault();
+		output.addType('txt', TextFileFormat.LINES);
+		output.addType('hxs', TextFileFormat.PLAINTEXT);
+		return output;
 	}
 }
 #end
