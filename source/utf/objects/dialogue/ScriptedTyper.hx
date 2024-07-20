625e0bbbdf78412f9b828cfbc14eafeed90f9374
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-21 00:11:09 +0300

    Create ScriptedTyper.hx

---
+++ source/utf/objects/dialogue/ScriptedTyper.hx
@@ -0,0 +1,10 @@
+package utf.objects.battle;
+
+import polymod.hscript.HScriptedClass;
+import utf.objects.dialogue.Typer;
+
+/**
+ * A dialogue typer that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedTyper extends Typer implements HScriptedClass {}
