ffe454d459c539846c5cf96b41a3eb581c345c86
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-20 15:29:36 +0300

    Create ScriptedRoomObject.hx

---
+++ source/utf/objects/room/ScriptedRoomObject.hx
@@ -0,0 +1,10 @@
+package utf.objects.room;
+
+import polymod.hscript.HScriptedClass;
+import utf.objects.room.RoomObject;
+
+/**
+ * A room object that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedRoomObject extends RoomObject implements HScriptedClass {}
