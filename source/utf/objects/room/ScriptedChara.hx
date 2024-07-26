9bc24bb7246fa8d1f7ac722ae07ad5176be55284
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-26 22:01:06 +0300

    Create ScriptedChara.hx

---
+++ source/utf/objects/room/ScriptedChara.hx
@@ -0,0 +1,10 @@
+package utf.objects.room;
+
+import polymod.hscript.HScriptedClass;
+import utf.objects.room.Chara;
+
+/**
+ * A room character that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedChara extends Chara implements HScriptedClass {}
