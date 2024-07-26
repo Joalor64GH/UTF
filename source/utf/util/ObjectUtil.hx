a26dee299f71b49174211906664b4d39a902a401
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-26 21:50:17 +0300

    Update ObjectUtil.hx

--- source/utf/util/ObjectUtil.hx
+++ source/utf/util/ObjectUtil.hx
@@ -9,23 +9,23 @@ import flixel.util.FlxAxes;
 class ObjectUtil
 {
 	/**
-	 * Calculates the bottom position of the given FlxObject.
-	 * @param obj The FlxObject instance.
-	 * @return The bottom position (y + height) or 0 if the object is null.
-	 */
-	public static function getBottom(obj:FlxObject):Float
-	{
-		return obj == null ? 0 : (obj.y + obj.height);
-	}
-
-	/**
 	 * Calculates the right position of the given FlxObject.
 	 * @param obj The FlxObject instance.
 	 * @return The right position (x + width) or 0 if the object is null.
 	 */
-	public static function getRight(obj:FlxObject):Float
+	public static inline function getRight(obj:FlxObject):Float
 	{
-		return obj == null ? 0 : (obj.x + obj.width);
+		return obj != null ? (obj.x + obj.width) : 0;
+	}
+
+	/**
+	 * Calculates the bottom position of the given FlxObject.
+	 * @param obj The FlxObject instance.
+	 * @return The bottom position (y + height) or 0 if the object is null.
+	 */
+	public static inline function getBottom(obj:FlxObject):Float
+	{
+		return obj != null ? (obj.y + obj.height) : 0;
 	}
 
 	/**
