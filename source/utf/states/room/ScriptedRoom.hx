8d2e448b3536104265a8eb8c56fbe1f01af16eb5
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-27 13:34:45 +0300

    Create ScriptedRoom.hx

---
+++ source/utf/states/room/ScriptedRoom.hx
@@ -0,0 +1,10 @@
+package utf.states.room;
+
+import polymod.hscript.HScriptedClass;
+import utf.states.room.Room;
+
+/**
+ * A room that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedRoom extends Room implements HScriptedClass {}
