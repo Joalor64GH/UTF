cb497d61ae4f00ae11af357732dc6d6fb1f72245
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-27 23:41:31 +0300

    Update Intro.hx

--- source/utf/states/Intro.hx
+++ source/utf/states/Intro.hx
@@ -8,7 +8,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import openfl.Lib;
-// import utf.backend.registries.RoomRegistry;
+import utf.backend.registries.RoomRegistry;
 import utf.backend.AssetPaths;
 import utf.input.Controls;
 import utf.backend.Data;
@@ -17,7 +17,6 @@ import utf.backend.Global;
 import utf.util.macro.git.GitUtil;
 #end
 import utf.states.room.Room;
-// import utf.states.room.ScriptedRoom;
 import utf.states.Settings;
 #if debug
 import utf.substates.battle.EnemyEncounter;
@@ -204,7 +203,7 @@ class Intro extends FlxState
 			switch (choices[selected])
 			{
 				case 'Continue':
-					// FlxG.switchState(() -> ScriptedRoom.init(RoomRegistry.roomClasses.get(272)));
+					FlxG.switchState(() -> RoomRegistry.fetchRoom(272));
 				case 'Begin Game':
 					openSubState(new Naming());
 				case 'Settings':
