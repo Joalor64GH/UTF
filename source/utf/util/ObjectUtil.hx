bccbd9873b125ed8bc3a2c4aac5eb982526510ba
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-28 16:53:15 +0300

    Update ObjectUtil.hx

--- source/utf/util/ObjectUtil.hx
+++ source/utf/util/ObjectUtil.hx
@@ -1,7 +1,6 @@
 package utf.util;
 
 import flixel.FlxObject;
-import flixel.util.FlxAxes;
 
 /**
  * Utility class for performing operations on FlxObject instances.
@@ -26,24 +25,5 @@ class ObjectUtil
 	public static inline function getBottom(obj:FlxObject):Float
 	{
 		return obj != null ? (obj.y + obj.height) : 0;
-	}
-
-	/**
-	 * Centers one FlxObject within another along specified axes.
-	 * @param obj The FlxObject to center.
-	 * @param base The base FlxObject to center within.
-	 * @param axes The axes along which to center (default is both X and Y).
-	 * @return The centered FlxObject.
-	 */
-	public static function centerObject(obj:FlxObject, base:FlxObject, ?axes:FlxAxes = FlxAxes.XY):Void
-	{
-		if (obj == null || base == null)
-			return;
-
-		if (axes.x)
-			obj.x = base.x + (base.width - obj.width) / 2;
-
-		if (axes.y)
-			obj.y = base.y + (base.height - obj.height) / 2;
 	}
 }
