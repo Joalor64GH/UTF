81c034bb5224e6236aac59f4f6e92a4ef602b6ad
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-01 14:59:44 +0300

    Update FlxGraphicUtil.hx

--- source/utf/util/FlxGraphicUtil.hx
+++ source/utf/util/FlxGraphicUtil.hx
@@ -23,23 +23,16 @@ class FlxGraphicUtil
 	 */
 	public static function fromRegion(graphic:FlxGraphicAsset, region:FlxRect, ?unique:Bool = false, ?key:String):FlxGraphic
 	{
-		// Add the graphic to the FlxG bitmap cache
 		final graph:FlxGraphic = FlxG.bitmap.add(graphic, unique, key);
 
-		// Return null if the graphic or its bitmap is invalid
 		if (graph == null || graph.bitmap == null)
 			return null;
 
-		// Create a new BitmapData object with the dimensions of the specified region
 		final portion:BitmapData = new BitmapData(Math.floor(region.width), Math.floor(region.height), true, 0);
-
-		// Copy pixels from the specified region of the original graphic to the new BitmapData object
 		portion.copyPixels(graph.bitmap, new Rectangle(region.x, region.y, region.width, region.height), new Point(0, 0));
 
-		// Release the FlxRect back to the object pool
 		region.put();
 
-		// Return a new FlxGraphic created from the BitmapData portion
 		return FlxGraphic.fromBitmapData(portion);
 	}
 }
