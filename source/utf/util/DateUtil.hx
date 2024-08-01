ae9fb7902708a8556935d367fb5bb7a8fede0249
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-01 09:43:45 +0300

    Update DateUtil.hx

--- source/utf/util/DateUtil.hx
+++ source/utf/util/DateUtil.hx
@@ -14,18 +14,36 @@ class DateUtil
 	{
 		final curDate:Date = Date.now();
 
-		switch (curDate.getMonth() + 1)
+		if (curDate != null)
 		{
-			case 12 | 1 | 2:
-				return 1;
-			case 3 | 4 | 5:
-				return 2;
-			case 6 | 7 | 8:
-				return 3;
-			case 9 | 10 | 11:
-				return 4;
+			switch (curDate.getMonth() + 1)
+			{
+				case 12 | 1 | 2:
+					return 1;
+				case 3 | 4 | 5:
+					return 2;
+				case 6 | 7 | 8:
+					return 3;
+				case 9 | 10 | 11:
+					return 4;
+			}
 		}
 
 		return 0;
+	}
+
+	/**
+	 * Get the current date and time formatted as a string suitable for file names.
+	 *
+	 * @return The current date and time in the format "YYYY-MM-DD_HH-MM-SS", or null if the date cannot be retrieved.
+	 */
+	public static function getFormattedDateTimeForFile():String
+	{
+		final curDate:Date = Date.now();
+
+		if (curDate != null)
+			return curDate.toString().replace(' ', '_').replace(':', '-');
+
+		return null;
 	}
 }
