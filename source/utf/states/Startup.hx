f9689711f7681f76e8549393f53e3d9d83c383f6
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-02 01:39:54 +0300

    Update Startup.hx

--- source/utf/states/Startup.hx
+++ source/utf/states/Startup.hx
@@ -1,14 +1,8 @@
 package utf.states;
 
-import flixel.addons.transition.FlxTransitionableState;
-import flixel.addons.transition.TransitionData;
-import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxState;
 import utf.backend.Data;
-#if hxdiscord_rpc
-import utf.backend.Discord;
-#end
 import utf.backend.Global;
 #if polymod
 import utf.modding.PolymodHandler;
@@ -19,25 +13,15 @@ class Startup extends FlxState
 {
 	override function create():Void
 	{
-		Data.load();
+		FlxG.autoPause = false;
 
-		#if hxdiscord_rpc
-		Discord.load();
-		#end
+		Data.load();
 
 		Global.load();
 
 		#if polymod
 		PolymodHandler.load();
 		#end
-
-		if (Data.settings.get('filter') != 'none' && Data.filters.exists(Data.settings.get('filter')))
-			FlxG.game.setFilters([Data.filters.get(Data.settings.get('filter'))]);
-
-		FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 0.5, NEW);
-		FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.5, NEW);
-
-		super.create();
 
 		FlxG.switchState(() -> new Title());
 	}
