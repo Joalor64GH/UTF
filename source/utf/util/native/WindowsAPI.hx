85ef92d1d1416e9f9e62641108f7467a154929d2
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-01 22:47:08 +0300

    Update WindowsAPI.hx

--- source/utf/util/native/WindowsAPI.hx
+++ source/utf/util/native/WindowsAPI.hx
@@ -21,50 +21,6 @@ package utf.util.native;
 class WindowsAPI
 {
 	/**
-	 * Shows a message box with an error icon.
-	 * @param message The message to display.
-	 * @param title The title of the message box.
-	 * @return The result of the message box interaction.
-	 */
-	public static function showError(message:cpp.ConstCharStar, title:cpp.ConstCharStar):Int
-	{
-		return untyped MessageBox(null, message, title, untyped MB_OK | untyped MB_ICONERROR);
-	}
-
-	/**
-	 * Shows a message box with a warning icon.
-	 * @param message The message to display.
-	 * @param title The title of the message box.
-	 * @return The result of the message box interaction.
-	 */
-	public static function showWarning(message:cpp.ConstCharStar, title:cpp.ConstCharStar):Int
-	{
-		return untyped MessageBox(null, message, title, untyped MB_OK | untyped MB_ICONWARNING);
-	}
-
-	/**
-	 * Shows a message box with an information icon.
-	 * @param message The message to display.
-	 * @param title The title of the message box.
-	 * @return The result of the message box interaction.
-	 */
-	public static function showInformation(message:cpp.ConstCharStar, title:cpp.ConstCharStar):Int
-	{
-		return untyped MessageBox(null, message, title, untyped MB_OK | untyped MB_ICONINFORMATION);
-	}
-
-	/**
-	 * Shows a message box with a question icon.
-	 * @param message The message to display.
-	 * @param title The title of the message box.
-	 * @return The result of the message box interaction.
-	 */
-	public static function showQuestion(message:cpp.ConstCharStar, title:cpp.ConstCharStar):Int
-	{
-		return untyped MessageBox(null, message, title, untyped MB_OKCANCEL | untyped MB_ICONQUESTION);
-	}
-
-	/**
 	 * Disables the "Report to Microsoft" dialog that appears when the application crashes.
 	 */
 	public static function disableErrorReporting():Void
