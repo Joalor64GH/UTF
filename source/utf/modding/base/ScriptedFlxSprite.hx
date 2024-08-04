c5cf1c211d4ebdf81975e7121e8ebee8834f517e
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-04 23:30:21 +0300

    Create ScriptedFlxSprite.hx

---
+++ source/utf/modding/base/ScriptedFlxSprite.hx
@@ -0,0 +1,10 @@
+package utf.modding.base;
+
+import flixel.FlxSprite;
+import polymod.hscript.HScriptedClass;
+
+/**
+ * A sprite that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedFlxSprite extends FlxSprite implements HScriptedClass {}
