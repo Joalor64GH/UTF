ee2d4b46bd0cfa15eb8edc1dfc11f0a7b9703808
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-05 12:45:42 +0300

    Update ScriptedFlxEmitter.hx

--- source/utf/modding/base/ScriptedFlxEmitter.hx
+++ source/utf/modding/base/ScriptedFlxEmitter.hx
@@ -4,4 +4,4 @@ package utf.modding.base;
  * A particles emitter that can be controlled by scripts.
  */
 @:hscriptClass
-class ScriptedFlxEmitter extends flixel.effects.particles.FlxEmitter implements HScriptedClass {}
+class ScriptedFlxEmitter extends flixel.effects.particles.FlxTypedEmitter<flixel.effects.particles.FlxParticle> implements HScriptedClass {}
