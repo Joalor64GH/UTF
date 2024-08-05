a2ba2b9f3173b734c293fef823781a3189ab7b73
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-05 09:57:37 +0300

    Create ScriptedFlxSpriteGroup.hx

---
+++ source/utf/modding/base/ScriptedFlxSpriteGroup.hx
@@ -0,0 +1,7 @@
+package funkin.modding.base;
+
+/**
+ * A sprite group that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedFlxSpriteGroup extends flixel.group.FlxTypedSpriteGroup<flixel.FlxSprite> implements HScriptedClass {}
