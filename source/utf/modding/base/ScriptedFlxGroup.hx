edfec3edf4b61ca461bccbc23460db073a334afe
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-05 12:47:39 +0300

    Update ScriptedFlxGroup.hx

--- source/utf/modding/base/ScriptedFlxGroup.hx
+++ source/utf/modding/base/ScriptedFlxGroup.hx
@@ -4,4 +4,4 @@ package utf.modding.base;
  * A group that can be controlled by scripts.
  */
 @:hscriptClass
-class ScriptedFlxGroup extends flixel.group.FlxGroup implements HScriptedClass {}
+class ScriptedFlxGroup extends flixel.group.FlxTypedGroup<flixel.FlxBasic> implements HScriptedClass {}
