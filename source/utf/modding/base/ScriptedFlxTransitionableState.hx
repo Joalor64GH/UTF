82836795622722b52cdea485c03ffd475a34e9b2
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-05 09:56:01 +0300

    Create ScriptedFlxTransitionableState.hx

---
+++ source/utf/modding/base/ScriptedFlxTransitionableState.hx
@@ -0,0 +1,7 @@
+package funkin.modding.base;
+
+/**
+ * A transitionable state that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedFlxTransitionableState extends flixel.addons.transition.FlxTransitionableState implements HScriptedClass {}
