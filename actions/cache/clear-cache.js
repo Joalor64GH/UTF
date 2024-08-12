7e3e1a0b2e3243ec6b6bb12854b2010a9392693b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-12 22:18:53 +0300

    Create clear-cache.js

---
+++ actions/cache/clear-cache.js
@@ -0,0 +1,29 @@
+module.exports = async ({github, context}, cacheKey) => {
+	try
+	{
+		const caches = await github.rest.actions.getActionsCacheList({
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+		});
+
+		for (const cache of caches.data.actions_caches)
+		{
+			if (cache.key === cacheKey)
+			{
+				console.log('Clearing ' + cache.key + '...');
+
+				await github.rest.actions.deleteActionsCacheById({
+					owner: context.repo.owner,
+					repo: context.repo.repo,
+					cache_id: cache.id,
+				});
+
+				console.log('Previous Cache Cleared!');
+			}
+		}
+	}
+	catch (error)
+	{
+		console.log(error.message);
+	}
+};
