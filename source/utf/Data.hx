19a2d2d600d2311969fd6fd9b55be7fc7db3d61c
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-13 00:25:03 +0300

    Update Data.hx

--- source/utf/Data.hx
+++ source/utf/Data.hx
@@ -3,24 +3,39 @@ package utf;
 import flixel.util.FlxSave;
 import flixel.FlxG;
 import openfl.Lib;
+import utf.util.FilterUtil;
 
+/**
+ * Handles saving and loading of settings to and from a file.
+ */
 class Data
 {
+	/**
+	 * Map to store game settings.
+	 */
 	public static var settings(default, null):Map<String, Dynamic> = [
 		'filter' => 'none'
 	];
 
+	/**
+	 * Saves the current settings to a file.
+	 * Uses the FlxSave class to bind to a file and stores the settings map.
+	 */
 	public static function save():Void
 	{
-		var save:FlxSave = new FlxSave();
+		final save:FlxSave = new FlxSave();
 		save.bind('data', Lib.application.meta.get('file'));
 		save.data.settings = settings;
 		save.close();
 	}
 
+	/**
+	 * Loads settings from a file and applies the saved filter.
+	 * Retrieves the settings using FlxSave and updates the settings map.
+	 */
 	public static function load():Void
 	{
-		var save:FlxSave = new FlxSave();
+		final save:FlxSave = new FlxSave();
 
 		save.bind('data', Lib.application.meta.get('file'));
 
@@ -28,6 +43,8 @@ class Data
 		{
 			if (save.data.settings != null)
 				settings = save.data.settings;
+
+			FilterUtil.reloadGameFilter(settings.get('filter'));
 		}
 
 		save.destroy();
