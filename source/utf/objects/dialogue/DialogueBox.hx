2c36f7bb487149220745cfaef718f43cdfc5e2e7
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-14 23:32:45 +0300

    Create DialogueBox.hx

---
+++ source/utf/objects/dialogue/DialogueBox.hx
@@ -0,0 +1,26 @@
+package utf.objects.dialogue;
+
+import flixel.addons.display.shapes.FlxShapeBox;
+import flixel.util.FlxColor;
+import objects.dialogue.Writer;
+
+class DialogueBox extends FlxSpriteGroup
+{
+	public var writer(default, null):Writer;
+
+	@:noCompletion
+	private var box:FlxShapeBox;
+
+	public function new(showOnTop:Bool = false):Void
+	{
+		box = new FlxShapeBox(32, showOnTop ? 10 : 320, 576, 150, {thickness: 6, jointStyle: MITER, color: FlxColor.WHITE},
+			FlxColor.BLACK);
+		box.scrollFactor.set();
+		box.active = false;
+		add(box);
+
+		writer = new Writer(box.x + 20, box.y + 10);
+		writer.scrollFactor.set();
+		add(writer);
+	}
+}
