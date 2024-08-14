eeff3710b6041cf4e456f7c2491084ee0e190c1d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-14 23:21:55 +0300

    Create PercentOfHeightScaleMode.hx

---
+++ source/utf/PercentOfHeightScaleMode.hx
@@ -0,0 +1,32 @@
+package utf;
+
+import flixel.system.scaleModes.RatioScaleMode;
+
+/**
+ * Custom scale mode that scales the game size based on a specified percentage of the original size.
+ */
+class PercentOfHeightScaleMode extends RatioScaleMode
+{
+	/**
+	 * The percentage by which to scale the game size.
+	 */
+	private var percent:Float;
+
+	/**
+	 * Creates a new `PercentOfHeightScaleMode` instance.
+	 * @param percent The percentage by which to scale the game size.
+	 */
+	public function new(percent:Float):Void
+	{
+		super();
+
+		this.percent = percent;
+	}
+
+	public override function updateScaleOffset():Void
+	{
+		gameSize.scale(percent);
+
+		super.updateScaleOffset();
+	}
+}
