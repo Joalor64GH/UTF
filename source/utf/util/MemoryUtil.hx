93a7f60f336ecb311882fd8fc7d9f38427462ffe
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-15 05:08:43 +0300

    Update MemoryUtil.hx

--- source/utf/util/MemoryUtil.hx
+++ source/utf/util/MemoryUtil.hx
@@ -16,7 +16,7 @@ class MemoryUtil
 	/**
 	 * Enables garbage collection.
 	 */
-	public static function enable():Void
+	public static inline function enable():Void
 	{
 		#if (cpp || hl)
 		Gc.enable(true);
@@ -26,7 +26,7 @@ class MemoryUtil
 	/**
 	 * Disables garbage collection.
 	 */
-	public static function disable():Void
+	public static inline function disable():Void
 	{
 		#if (cpp || hl)
 		Gc.enable(false);
@@ -37,7 +37,7 @@ class MemoryUtil
 	 * Runs garbage collection. Should be called from the main thread.
 	 * @param major Set to true to perform a major collection.
 	 */
-	public static function collect(?major:Bool = false):Void
+	public static inline function collect(?major:Bool = false):Void
 	{
 		#if (cpp || neko)
 		Gc.run(major);
@@ -48,7 +48,7 @@ class MemoryUtil
 	 * Performs repeated major garbage collection until less than 16KB is freed in one operation.
 	 * Should be called from the main thread.
 	 */
-	public static function compact():Void
+	public static inline function compact():Void
 	{
 		#if cpp
 		Gc.compact();
