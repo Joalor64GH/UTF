bdd236c0b73c7fde29fb6590b812e3b26e8ca60b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-21 18:19:55 +0300

    Update Assets.hx

--- source/utf/Assets.hx
+++ source/utf/Assets.hx
@@ -3,7 +3,9 @@ package utf;
 import flixel.FlxG;
 import haxe.io.Path;
 import haxe.Exception;
+import openfl.display.BitmapData;
 import openfl.media.Sound;
+import openfl.text.Font;
 import openfl.utils.Assets as OpenFLAssets;
 import openfl.utils.ByteArray;
 
