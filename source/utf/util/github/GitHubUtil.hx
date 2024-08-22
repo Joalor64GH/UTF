6f9e3820e9699bf25508bd84b1c5c6d2c5f6f568
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-22 08:45:39 +0300

    Update GitHubUtil.hx

--- source/utf/util/github/GitHubUtil.hx
+++ source/utf/util/github/GitHubUtil.hx
@@ -10,6 +10,32 @@ import utf.util.github.Contributor;
 using StringTools;
 
 /**
+ * Represents a GitHub contributor.
+ */
+typedef Contributor =
+{
+	login:String,
+	id:Int,
+	node_id:String,
+	avatar_url:String,
+	gravatar_id:String,
+	url:String,
+	html_url:String,
+	followers_url:String,
+	following_url:String,
+	gists_url:String,
+	starred_url:String,
+	subscriptions_url:String,
+	organizations_url:String,
+	repos_url:String,
+	events_url:String,
+	received_events_url:String,
+	type:String,
+	site_admin:Bool,
+	contributions:Int
+}
+
+/**
  * Utility class for interacting with the GitHub API.
  */
 class GitHubUtil
