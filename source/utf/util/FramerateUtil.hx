a0c4e94619ead63359d6bd887d1c5b82db4b130d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-24 16:55:41 +0300

    Update FramerateUtil.hx

--- source/utf/util/FramerateUtil.hx
+++ source/utf/util/FramerateUtil.hx
@@ -9,6 +9,13 @@ import openfl.Lib;
 class FramerateUtil
 {
 	/**
+	 * The timing of a single frame.
+	 *
+	 * @see https://github.com/LavaStudios/CreateYourDroid/blob/4a5001b16f399a070d1f8d0c1256fba9bb9764ca/Assets/Scripts/Text/TextManager.cs#L72
+	 */
+	public static final SINGLE_FRAME_TIMING:Float = 1.0 / 20;
+
+	/**
 	 * Adjusts the stage framerate to match the display's refresh rate.
 	 * If the refresh rate is below 60 Hz, the framerate is set to 60 FPS.
 	 * This adjustment does not influence Flixel's `FlxGame` framerate.
