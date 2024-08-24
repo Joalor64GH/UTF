2fab4e343ad44e2f65d8f8c59f050c3634edd26f
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-24 22:47:15 +0300

    Create ScriptedPortrait.hx

---
+++ source/utf/objects/dialogue/portraits/ScriptedPortrait.hx
@@ -0,0 +1,10 @@
+package utf.objects.dialogue.typers;
+
+import polymod.hscript.HScriptedClass;
+import utf.objects.dialogue.portraits.Portrait;
+
+/**
+ * A dialogue portrait that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedPortrait extends Portrait implements HScriptedClass {}
