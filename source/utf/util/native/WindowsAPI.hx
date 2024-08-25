4f880929e206ca1029078969e3140d267975f9f1
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-25 11:29:35 +0300

    Update WindowsAPI.hx

--- source/utf/util/native/WindowsAPI.hx
+++ source/utf/util/native/WindowsAPI.hx
@@ -2,19 +2,27 @@ package utf.util.native;
 
 #if (windows && cpp)
 /**
+ * Contains extended memory statistics for a process.
+ */
+@:build(utf.external.Linker.xml('builders/WindowsAPIBuilder.xml'))
+@:cppInclude('windows.h')
+@:cppInclude('psapi.h')
+@:unreflective
+@:structAccess
+@:native('PROCESS_MEMORY_COUNTERS_EX')
+extern class PROCESS_MEMORY_COUNTERS_EX
+{
+	@:native('PROCESS_MEMORY_COUNTERS_EX')
+	static function alloc():PROCESS_MEMORY_COUNTERS_EX;
+
+	var PeakWorkingSetSize:cpp.SizeT;
+	var WorkingSetSize:cpp.SizeT;
+}
+
+/**
  * Utility class for Windows API-related functions.
  */
-@:buildXml('
-<target id="haxe">
-	<section if="mingw">
-		<lib name="-lpsapi" />
-	</section>
-
-	<section if="windows" unless="mingw">
-		<lib name="psapi.lib" />
-	</section>
-</target>
-')
+@:build(utf.external.Linker.xml('builders/WindowsAPIBuilder.xml'))
 @:cppInclude('windows.h')
 @:cppInclude('psapi.h')
 @:nativeGen
@@ -70,6 +78,42 @@ class WindowsAPI
 	public static function disableErrorReporting():Void
 	{
 		untyped SetErrorMode(untyped SEM_FAILCRITICALERRORS | untyped SEM_NOGPFAULTERRORBOX);
+	}
+
+	/**
+	 * Retrieves the current working set size of the process.
+	 * This represents the amount of physical memory (RAM) currently being used by the process.
+	 * @return The current working set size in bytes. Returns 0 if the retrieval fails.
+	 */
+	public static function GetProcessMemoryWorkingSetSize():cpp.SizeT
+	{
+		final pcm:PROCESS_MEMORY_COUNTERS_EX = PROCESS_MEMORY_COUNTERS_EX.alloc();
+
+		if (untyped GetProcessMemoryInfo(untyped GetCurrentProcess(), untyped __cpp__('(PROCESS_MEMORY_COUNTERS *) {0}', cpp.RawPointer.addressOf(pcm)),
+			untyped sizeof(pmc)))
+			return pmc.WorkingSetSize;
+		else
+			Sys.println('Failed to get memory usage information.');
+
+		return 0;
+	}
+
+	/**
+	 * Retrieves the peak working set size of the process.
+	 * This represents the maximum amount of physical memory (RAM) used by the process at any point in its lifetime.
+	 * @return The peak working set size in bytes. Returns 0 if the retrieval fails.
+	 */
+	public static function GetProcessMemoryPeakWorkingSetSize():cpp.SizeT
+	{
+		final pcm:PROCESS_MEMORY_COUNTERS_EX = PROCESS_MEMORY_COUNTERS_EX.alloc();
+
+		if (untyped GetProcessMemoryInfo(untyped GetCurrentProcess(), untyped __cpp__('(PROCESS_MEMORY_COUNTERS *) {0}', cpp.RawPointer.addressOf(pcm)),
+			untyped sizeof(pmc)))
+			return pmc.PeakWorkingSetSize;
+		else
+			Sys.println('Failed to get peak working set size information.');
+
+		return 0;
 	}
 }
 #end
