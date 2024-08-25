bf0c249f8dd3cf19fa594c12621ea46214ba1bfc
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-08-25 12:03:16 +0300

    Update Settings.hx

--- source/utf/states/config/Settings.hx
+++ source/utf/states/config/Settings.hx
@@ -218,7 +218,7 @@ class Settings extends FlxState
 
 		tobdogLine.centerOffsets();
 
-		tobdogLine.offset.add(Math.sin((FlxG.game.ticks / 100) / 12), Math.cos((FlxG.game.ticks / 100) / 12));
+		tobdogLine.offset.add(Math.sin((FramerateUtil.SINGLE_FRAME_TIMING * FlxG.game.ticks) / 12), Math.cos((FramerateUtil.SINGLE_FRAME_TIMING * FlxG.game.ticks) / 12));
 	}
 
 	@:noCompletion
