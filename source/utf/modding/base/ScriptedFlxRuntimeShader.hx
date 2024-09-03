115cc7880b1a758e3c907a4cfe8018202c085e54
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-03 23:00:21 +0300

    Create ScriptedFlxRuntimeShader.hx

---
+++ source/utf/modding/base/ScriptedFlxRuntimeShader.hx
@@ -0,0 +1,7 @@
+package utf.modding.base;
+
+/**
+ * A shader that can be controlled by scripts.
+ */
+@:hscriptClass
+class ScriptedFlxRuntimeShader extends flixel.addons.display.FlxRuntimeShader implements HScriptedClass {}
